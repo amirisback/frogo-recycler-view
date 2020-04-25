cc06d98f70fa878ffcb4142ccce177ad420eefca
amirisback <faisalamircs@gmail.com>
2020-04-26 05:00:11 +0700

    add-dev: Development multi view layout

---
+++ frogorecyclerview/src/main/java/com/frogobox/recycler/boilerplate/holder/FrogoMultiViewHolder.kt
@@ -0,0 +1,19 @@
+package com.frogobox.recycler.boilerplate.holder
+
+/*
+ * Created by Faisal Amir
+ * =========================================
+ * FrogoRecyclerViewAdapter
+ * Copyright (C) 26/04/2020.      
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * FrogoBox Inc
+ * com.frogobox.recycler
+ * 
+ */
+class FrogoMultiViewHolder {
+}
