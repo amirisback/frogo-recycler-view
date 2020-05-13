3c6297ab30f430189524c9f65bec8c63cf16306b
amirisback <faisalamircs@gmail.com>
2020-05-13 08:32:47 +0700

    update: build.gradle

--- frogorecyclerview/src/androidTest/java/com/frogobox/recycler/ExampleInstrumentedTest.kt
+++ frogorecyclerview/src/androidTest/java/com/frogobox/recycler/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.frogobox.frogoviewadapter.test", appContext.packageName)
+        assertEquals("com.frogobox.frogoRecyclerView.test", appContext.packageName)
     }
 }
