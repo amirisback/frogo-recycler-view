a097d9cb49937a66d089bef1795da1aa156de18a
amirisback <faisalamircs@gmail.com>
2020-06-03 10:09:29 +0700

    add: sample fetch api using shimmer recycler view

--- app/src/main/java/com/frogobox/recycler/MainDevActivity.kt
+++ app/src/main/java/com/frogobox/recycler/MainDevActivity.kt
@@ -1,8 +1,15 @@
 package com.frogobox.recycler
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import com.frogobox.frogonewsapi.ConsumeNewsApi
+import com.frogobox.frogonewsapi.callback.NewsResultCallback
+import com.frogobox.frogonewsapi.data.model.Article
+import com.frogobox.frogonewsapi.data.response.ArticleResponse
+import com.frogobox.frogonewsapi.util.NewsConstant
+import com.frogobox.frogonewsapi.util.NewsUrl
 import com.frogobox.recycler.base.BaseActivity
 import com.frogobox.recycler.boilerplate.viewrclass.FrogoViewAdapterCallback
 import com.frogobox.recycler.model.ExampleModel
@@ -13,7 +20,7 @@ class MainDevActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainDevBinding.root)
-        setupFrogoShimmerRecyclerView()
+        setupNewsApi()
         setupButtonShimmer()
     }
 
@@ -26,32 +33,32 @@ class MainDevActivity : BaseActivity() {
         return listString
     }
 
-    private fun setupFrogoShimmerRecyclerView() {
+    private fun setupFrogoShimmerRecyclerView(data: List<Article>) {
 
         val adapterCallback = object :
-            FrogoViewAdapterCallback<ExampleModel> {
-            override fun setupInitComponent(view: View, data: ExampleModel) {
+            FrogoViewAdapterCallback<Article> {
+            override fun setupInitComponent(view: View, data: Article) {
                 // Init component content item recyclerview
-                view.findViewById<TextView>(R.id.frogo_rv_list_type_1_tv_title).text = data.name
+                view.findViewById<TextView>(R.id.frogo_rv_list_type_1_tv_title).text = data.title
             }
 
-            override fun onItemClicked(data: ExampleModel) {
+            override fun onItemClicked(data: Article) {
                 // setup item clicked on frogo recycler view
-                showToast(data.name)
+                data.title?.let { showToast(it) }
             }
 
-            override fun onItemLongClicked(data: ExampleModel) {
+            override fun onItemLongClicked(data: Article) {
                 // setup item long clicked on frogo recycler view
-                showToast(data.name)
+                data.title?.let { showToast(it) }
             }
         }
 
-        activityMainDevBinding.rvShimmer.injector<ExampleModel>()
-            .addData(listData())
+        activityMainDevBinding.rvShimmer.injector<Article>()
+            .addData(data)
             .addCustomView(R.layout.frogo_rv_list_type_1)
             .addEmptyView(null)
             .addShimmerViewPlaceHolder(R.layout.frogo_rv_list_type_1)
-            .addShimmerSumOfItemLoading(listData().size)
+            .addShimmerSumOfItemLoading(data.size)
             .addCallback(adapterCallback)
             .createLayoutLinearVertical(false)
             .build()
@@ -71,6 +78,49 @@ class MainDevActivity : BaseActivity() {
             bool = !bool
             setupShimmer(bool)
         }
+    }
+
+    private fun setupNewsApi(){
+        val consumeNewsApi = ConsumeNewsApi(NewsUrl.NEWS_API_KEY)
+        consumeNewsApi.usingChuckInterceptor(this)
+        consumeNewsApi.getTopHeadline( // Adding Base Parameter on main function
+            null,
+            null,
+            null,
+            NewsConstant.COUNTRY_ID,
+            null,
+            null,
+            object : NewsResultCallback<ArticleResponse> {
+                override fun getResultData(data: ArticleResponse) {
+                    // Your Ui or data
+                    data.articles?.let { setupFrogoShimmerRecyclerView(it) }
+                }
+
+                override fun failedResult(statusCode: Int, errorMessage: String?) {
+                    // Your failed to do
+                    errorMessage?.let { showToast(it) }
+                }
+
+                override fun onShowProgress() {
+                    // Your Progress Show
+                    Log.d("RxJavaShow", "Show Progress")
+                    runOnUiThread {
+                        // Stuff that updates the UI
+                        activityMainDevBinding.rvShimmer.startShimmer()
+                    }
+                }
+
+                override fun onHideProgress() {
+                    // Your Progress Hide
+                    Log.d("RxJavaHide", "Hide Progress")
+                    runOnUiThread {
+                        // Stuff that updates the UI
+                        activityMainDevBinding.rvShimmer.stopShimmer()
+                    }
+
+                }
+
+            })
     }
 
 }
