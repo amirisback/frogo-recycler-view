929fe7117d9fe3c5ac9184d0e00862430c69c458
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-08-22 12:29:20 +0700

    add: FrogoRecyclerCompose.kt

---
+++ frogorecyclerview/src/main/java/com/frogobox/recycler/compose/FrogoRecyclerCompose.kt
@@ -0,0 +1,16 @@
+package com.frogobox.recycler.compose
+
+/*
+ * Created by faisalamir on 21/08/21
+ * FrogoRecyclerView
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * -----------------------------------------
+ * Copyright (C) 2021 FrogoBox Inc.      
+ * All rights reserved
+ *
+ */
+class FrogoRecyclerCompose {
+}
